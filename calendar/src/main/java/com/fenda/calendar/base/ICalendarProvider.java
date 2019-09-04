ccf5ffd9049910326e3b846e6fbdc94f71d51e40
David-lvfujiang <2411795294@qq.com>
2019-09-04 15:59:43 +0800

    日历组件和百科

--- calendar/src/main/java/com/fenda/calendar/base/ICalendarProvider.java
+++ calendar/src/main/java/com/fenda/calendar/base/ICalendarProvider.java
@@ -2,6 +2,11 @@ package com.fenda.calendar.base;
 
 import com.alibaba.android.arouter.facade.template.IProvider;
 
+/**
+ * @author LiFuJiang
+ * @Date 2019/8/29 16:51
+ * @Description 业务接口
+ */
 public interface ICalendarProvider extends IProvider {
 
     public void getCalendarMsg(String json);
