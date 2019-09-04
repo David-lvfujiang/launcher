3d89702a42a3196ff12c11899d4294296f065569
kevin.wangzhiqiang <kevin.wangzhiqiang@fenda.com>
2019-09-04 13:49:17 +0800

    音视频呼叫接口

--- call/src/main/java/com/fenda/call/service/CallService.java
+++ call/src/main/java/com/fenda/call/service/CallService.java
@@ -1,6 +1,7 @@
 package com.fenda.call.service;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.fenda.call.utils.ImConnectUtil;
@@ -9,6 +10,13 @@ import com.fenda.common.provider.ICallProvider;
 import com.fenda.common.router.RouterPath;
 import com.fenda.common.util.LogUtils;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import io.rong.callkit.RongCallKit;
 import io.rong.imkit.RongIM;
 
 /**
@@ -27,5 +35,17 @@ public class CallService implements ICallProvider {
     public void login(String rongCloudToken) {
         LogUtils.i("CallService login:" + rongCloudToken);
         ImConnectUtil.connectIm(rongCloudToken);
+    }
+
+    @Override
+    public void call(int callType, String callNumber) {
+        if (ImConnectUtil.isConectIm(BaseApplication.getInstance())) {
+            if (callType == 1) {
+                RongCallKit.startSingleCall(BaseApplication.getInstance(), callNumber, RongCallKit.CallMediaType.CALL_MEDIA_TYPE_VIDEO);
+                Log.d("TAG", "onClick: startSingleCall id" + callNumber);
+            } else if (callType == 0) {
+                RongCallKit.startSingleCall(BaseApplication.getInstance(), callNumber, RongCallKit.CallMediaType.CALL_MEDIA_TYPE_AUDIO);
+            }
+        }
     }
 }
--- common/src/main/java/com/fenda/common/provider/ICallProvider.java
+++ common/src/main/java/com/fenda/common/provider/ICallProvider.java
@@ -4,4 +4,5 @@ import com.alibaba.android.arouter.facade.template.IProvider;
 
 public interface ICallProvider extends IProvider {
     void login(String rongCloundToken);
+    void call(int callType, final String callNumber);
 }
