28c260b1b282f00be26b7b72711cc5b1f667961b
MattLjp <MattLjp@github.com>
2019-09-27 09:22:24 +0800

    闹钟接口

--- remind/src/main/java/com/fenda/remind/AlarmListActivity.java
+++ remind/src/main/java/com/fenda/remind/AlarmListActivity.java
@@ -72,6 +72,18 @@ public class AlarmListActivity extends BaseActivity {
         if (Constant.Remind.DELETE_REMIND.equals(alarmType)){
             tvAlarmEdit.setText("完成");
         }
+        if (alarmBeans == null){
+            alarmList.setVisibility(View.GONE);
+        }else {
+            alarmList.setVisibility(View.VISIBLE);
+            LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+            alarmList.setLayoutManager(manager);
+            alarmList.setItemAnimator(new DefaultItemAnimator());
+            mAdapter = new AlarmAdapter(this, alarmBeans);
+            alarmList.setAdapter(mAdapter);
+            addListener();
+        }
+        countDownTime();
         LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         alarmList.setLayoutManager(manager);
         alarmList.setItemAnimator(new DefaultItemAnimator());
@@ -145,6 +157,12 @@ public class AlarmListActivity extends BaseActivity {
         super.onNewIntent(intent);
         ArrayList<AlarmBean> mAlarmBean = intent.getParcelableArrayListExtra(Constant.Remind.ALARM_LIST);
         alarmType   = intent.getStringExtra(Constant.Remind.ALARM_TYPE);
+        if (mAlarmBean == null){
+            alarmList.setVisibility(View.GONE);
+        }else {
+            alarmList.setVisibility(View.VISIBLE);
+        }
+
         if (Constant.Remind.DELETE_REMIND.equals(alarmType)){
             if (alarmBeans == null){
                 alarmBeans = mAlarmBean;
