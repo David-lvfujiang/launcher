84af0b8f61148906649f719105da42d9f97287cb
David-lvfujiang <2411795294@qq.com>
2019-10-09 17:34:30 +0800

    百科修改滚动条自动关闭

--- encyclopedia/src/main/java/com/fenda/encyclopedia/view/EncyclopediaQuestiionActivity.java
+++ encyclopedia/src/main/java/com/fenda/encyclopedia/view/EncyclopediaQuestiionActivity.java
@@ -2,18 +2,19 @@ package com.fenda.encyclopedia.view;
 
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.RequiresApi;
 import android.text.Html;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.fenda.common.base.BaseActivity;
 import com.fenda.common.router.RouterPath;
+import com.fenda.common.util.LogUtil;
 import com.fenda.encyclopedia.R;
 
 /**
@@ -22,7 +23,8 @@ import com.fenda.encyclopedia.R;
  * @describe: 百科问答Activity
  */
 @Route(path = RouterPath.Encyclopedia.ENCYCLOPEDIA_QUESTIION_ACTIVITY)
-public class EncyclopediaQuestiionActivity extends BaseActivity implements View.OnClickListener, View.OnTouchListener {
+public class EncyclopediaQuestiionActivity extends BaseActivity implements View.OnClickListener, View.OnTouchListener, EncyclopediaAutoScrollView.ISmartScrollChangedListener {
+    private final int AUDIO_CONVERSE_CLOSE = 0;
     private TextView mTvContent, mTvTitle;
     private ImageView mImgReturnBack;
     private EncyclopediaAutoScrollView mAutoScrollView;
@@ -30,6 +32,18 @@ public class EncyclopediaQuestiionActivity extends BaseActivity implements View.
     String content;
     @Autowired
     String title;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case AUDIO_CONVERSE_CLOSE:    // 关闭界面
+                    EncyclopediaQuestiionActivity.this.finish();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     public int onBindLayout() {
@@ -43,7 +57,10 @@ public class EncyclopediaQuestiionActivity extends BaseActivity implements View.
         mAutoScrollView = findViewById(R.id.scrollView);
         mImgReturnBack = findViewById(R.id.title_return_img);
         mImgReturnBack.setOnClickListener(this);
+        //设置滑动监听
         mAutoScrollView.setOnTouchListener(this);
+        //设置滚动条在底部、顶部监听
+        mAutoScrollView.setmSmartScrollChangedListener(this);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -57,7 +74,7 @@ public class EncyclopediaQuestiionActivity extends BaseActivity implements View.
         //开始滚动时间
         mAutoScrollView.setFistTimeScroll(5000);
         //滚动的速率
-        mAutoScrollView.setScrollRate(50);
+        mAutoScrollView.setScrollRate(100);
         //是否循环滑动
         mAutoScrollView.setScrollLoop(false);
     }
@@ -100,6 +117,28 @@ public class EncyclopediaQuestiionActivity extends BaseActivity implements View.
 
         }
         return false;
+    }
+
+    /**
+     * 自动滑动到底部后回调方法
+     */
+    @Override
+    public void onScrolledToBottom() {
+        LogUtil.e("滑到底部");
+        LogUtil.e(mAutoScrollView.getAutoToScroll() + "");
+        //自动滚动到底部10秒后关闭activity
+        if (mAutoScrollView.getAutoToScroll() == true) {
+            handler.sendEmptyMessageDelayed(AUDIO_CONVERSE_CLOSE, 10000);
+        }
+    }
+
+    /**
+     * 内容不超过ScrollView时自动回调
+     */
+    @Override
+    public void onScrolledToTop() {
+        //12秒后自动关闭activity
+        handler.sendEmptyMessageDelayed(AUDIO_CONVERSE_CLOSE, 15000);
     }
 
 }
